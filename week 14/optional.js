2873c392b7fefd0e0a93ec59251c3e777bd5be5e
MINT <mint_chanit@hotmail.com>
2025-05-18 14:55:54 +0700

    feat: add week14 excercise

---
+++ week 14/optional.js
@@ -0,0 +1,17 @@
+// **Optional: Palindrome Checker**
+
+// - ให้เขียน Function ที่ชื่อว่า `isPalindrome` ซึ่งมีคุณสมบัติดังนี้
+//     - รับ Parameter 1 ตัวคือ `word` มี Value Type เป็น `String`
+//     - Function นี้จะ Return ค่า `true` ถ้า `word` เป็น Palindrome
+//     - หากไม่ใช่ ให้ Return `false`
+//     - Palindrome คือคำที่อ่านจากหน้าไปหลังหรือหลังไปหน้าก็ได้คำเดียวกัน เช่น `“madam”`, `“racecar”`
+
+function isPalindrome(word) {
+   //Start Coding Here
+};
+
+let result1 = isPalindrome("madam");
+console.log(result1); // true
+
+let result2 = isPalindrome("hello");
+console.log(result2); // false
